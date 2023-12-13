ea1c3bd9e24217ab706a76e48da1fda6f9c8572d
saintsappho <robinrosefleur@gmail.com>
2023-12-12 17:38:24 -0700

    20 through 23 all implemented, tested, proven.

--- 4_queries/23_total_cohort_asst_duration.sql
+++ 4_queries/23_total_cohort_asst_duration.sql
@@ -1,7 +1,9 @@
-SELECT  cohorts.name AS name, 
-        avg(completed_at-started_at) as average_assistance_time
+SELECT  
+      cohorts.name AS cohort, 
+      sum(completed_at-started_at) as total_duration
 FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON students.cohort_id = cohorts.id  
 GROUP BY cohorts.name
-ORDER BY average_assistance_time;
+ORDER BY total_duration;
+
