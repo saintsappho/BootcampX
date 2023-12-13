fb47b2fd89a976fdc72e1a85b650a80b2113fe0f
saintsappho <robinrosefleur@gmail.com>
2023-12-12 17:03:00 -0700

    important data by assistance request implemented

--- 4_queries/18_important_data_by_assistance.sql
+++ 4_queries/18_important_data_by_assistance.sql
@@ -1,5 +1,9 @@
-SELECT teachers.name AS teacher ,students.name AS student, assignments.name AS assignment, assistance_requests.duration AS duration
-FROM teachers
-JOIN students ON students.name = student_name
-JOIN assignments ON assignment_name = assignments.name
-JOIN assistance_requests ON assistance_requests_duration = assistance_requests.duration;
+SELECT teachers.name AS teacher,
+       students.name AS student,
+       assignments.name AS assignment,
+       (completed_at-started_at) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
